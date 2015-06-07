e9fefb48b2934189fcf64ab646f08c5398711f88
Roman Sklenář <mail@romansklenar.cz>
2015-06-07 22:12:59 +0200

    Make app work in Heroku's readonly environment

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,17 +4,18 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->enableDebugger(__DIR__ . '/../log');
-
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+$configurator->setDebugMode(TRUE); // enable for your remote IP
+$configurator->enableDebugger(__DIR__ . '/../tmp/log');
+$configurator->setTempDirectory(__DIR__ . '/../tmp');
 
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__)
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+if (file_exists($localConfig = __DIR__ . '/config/config.local.neon')) {
+  $configurator->addConfig($localConfig);
+}
 
 $container = $configurator->createContainer();
 
